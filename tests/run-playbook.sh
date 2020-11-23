9ae06bf18feab9412ea9459122b78dc34b224494
SanheHu <4038571439@GSA.GOV>
2020-11-22 23:52:45 -0500

    Update run-playbook.sh

--- tests/run-playbook.sh
+++ tests/run-playbook.sh
@@ -5,4 +5,4 @@ dir_repo_root="$( dirname ${dir_here} )"
 path_prepare_vars_python_script="${dir_here}/prepare_vars.py"
 
 python ${path_prepare_vars_python_script}
-ansible-playbook ${dir_here}/playbook.yml --extra-vars "role_dir=${dir_repo_root}"
+ansible-playbook ${dir_here}/playbook.yml --extra-vars "@${dir_here}/vars.json"
